e2e9988b8e75bfcef7418cdcee70062ea31e1fa5
AlbedoUniversal <Albedogforce@gmail.com>
2019-10-29 16:07:05 +0300

    mr.Magirko the best of teahcger about veb-def

--- app/js/main.js
+++ app/js/main.js
@@ -1,41 +1,44 @@
 window.onload = () => {
-  const cards = document.querySelectorAll(".cards-item");
-  const btns = document.querySelectorAll(".btn");
-  const cartBtn = document.querySelector(".cart-btn");
-  const dropDown = document.querySelector(".cart-dropdown");
-
-  // раздаем карточкам товара и кнопкам id
-  function appropriationDataAtrr() {
-    cards.forEach(function(card, index) {
-      card.setAttribute("data-key", `${index + 1}`);
-      card
-        .querySelector(".btn")
-        .setAttribute("data-product-id", `${index + 1}`);
-    });
-  }
-  appropriationDataAtrr();
-  // .....................................
-
-  // показать корзину
-  cartBtn.addEventListener("click", () => {
-    showCart();
-  });
-  function showCart() {
-    dropDown.classList.toggle("activeDrop");
-  }
-  // .......................................
-
-  function addToCart() {
-    btns.forEach(function(btn) {
-      btn.addEventListener("click", () => {
-        adderCart(btn);
-      });
-    });
-  }
-  addToCart();
-  function adderCart(buy) {
-    console.log(buy);
-  }
+  const card = document.querySelector(".cards-item");
+  console.log(card);
 };
 // 2 задача - сделать корзины иконку, как присылал, примерно так
 // в ней надо сделать заготовку dropdown, в который будут попадать товары при добавлении
+
+// const cards = document.querySelectorAll(".cards-item");
+// const btns = document.querySelectorAll(".btn");
+// const cartBtn = document.querySelector(".cart-btn");
+// const dropDown = document.querySelector(".cart-dropdown");
+
+// // раздаем карточкам товара и кнопкам id
+// function appropriationDataAtrr() {
+//   cards.forEach(function(card, index) {
+//     card.setAttribute("data-key", `${index + 1}`);
+//     card
+//       .querySelector(".btn")
+//       .setAttribute("data-product-id", `${index + 1}`);
+//   });
+// }
+// appropriationDataAtrr();
+// // .....................................
+
+// // показать корзину
+// cartBtn.addEventListener("click", () => {
+//   showCart();
+// });
+// function showCart() {
+//   dropDown.classList.toggle("activeDrop");
+// }
+// // .......................................
+
+// function addToCart() {
+//   btns.forEach(function(btn) {
+//     btn.addEventListener("click", () => {
+//       adderCart(btn);
+//     });
+//   });
+// }
+// addToCart();
+// function adderCart(buy) {
+//   console.log(buy);
+// }
