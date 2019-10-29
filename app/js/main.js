1ab064086bf29ac6ecee2127bf4da98f22ff348a
AlbedoUniversal <Albedogforce@gmail.com>
2019-10-29 12:46:04 +0300

    polufront

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,41 @@
-function appropriationDataAtrr() {
+window.onload = () => {
   const cards = document.querySelectorAll(".cards-item");
-  cards.forEach(function(card, index) {
-    const btns = card.querySelector(".btn");
-    card.setAttribute("data-key", `${index + 1}`);
-    btns.setAttribute("data-product-id", `${index + 1}`);
+  const btns = document.querySelectorAll(".btn");
+  const cartBtn = document.querySelector(".cart-btn");
+  const dropDown = document.querySelector(".cart-dropdown");
+
+  // раздаем карточкам товара и кнопкам id
+  function appropriationDataAtrr() {
+    cards.forEach(function(card, index) {
+      card.setAttribute("data-key", `${index + 1}`);
+      card
+        .querySelector(".btn")
+        .setAttribute("data-product-id", `${index + 1}`);
+    });
+  }
+  appropriationDataAtrr();
+  // .....................................
+
+  // показать корзину
+  cartBtn.addEventListener("click", () => {
+    showCart();
   });
-}
-appropriationDataAtrr();
+  function showCart() {
+    dropDown.classList.toggle("activeDrop");
+  }
+  // .......................................
 
-// 1  - написать ф-цию, которая расставляет случайные id после заргрузки страницы для каждой карточки и конпки добавления в корзину
-// <div class="cards-item" data-key="12">
-//       <button class="btn" data-product-id="12">buy now</button>
-//  </div>
-
+  function addToCart() {
+    btns.forEach(function(btn) {
+      btn.addEventListener("click", () => {
+        adderCart(btn);
+      });
+    });
+  }
+  addToCart();
+  function adderCart(buy) {
+    console.log(buy);
+  }
+};
 // 2 задача - сделать корзины иконку, как присылал, примерно так
 // в ней надо сделать заготовку dropdown, в который будут попадать товары при добавлении
