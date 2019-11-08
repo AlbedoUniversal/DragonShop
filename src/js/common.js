9b8e1c8eab11b9539856d969f4aebad16deb7610
AlbedoUniversal <Albedogforce@gmail.com>
2019-11-08 17:07:36 +0300

    front

--- src/js/common.js
+++ src/js/common.js
@@ -127,7 +127,6 @@ function activeDrop() {
 }
 
 function deleteThis(liId) {
-  // console.log(liId);
   arrCart.forEach(removed => {
     if (liId.id === removed.id) {
       console.log(`${liId}`);
